d77968c5afa85f50366dd77931d7b77fa6c3cd67
James Sumners <james.sumners@gmail.com>
2016-06-27 14:48:12 -0400

    Initial code commit

---
+++ pipeline.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const path = require('path')
+const spawn = require('child_process').spawn
+
+const nopt = require('nopt')
+const longOpts = {
+  config: String
+}
+const shortOpts = {
+  c: '--config'
+}
+const args = nopt(longOpts, shortOpts)
+
+if (!args.config) {
+  process.stderr.write('Must supply --config (-c) mappings with config file.')
+  process.exit(1)
+}
+
+let config
+try {
+  config = require(path.resolve(args.config))
+} catch (e) {
+  process.stderr.write('Could not load config file: ' + e.message)
+  process.exit(2)
+}
+
+let tail
+let input
+if (!config.input || config.input === 'stdin') {
+  input = process.stdin
+} else {
+  tail = spawn(config.input.bin, config.input.args)
+  input = tail.stdout
+}
+input.pipe(process.stdout)
+
+const outputs = []
+config.outputs.forEach((output) => {
+  const proc = spawn(output.bin, output.args)
+  input.pipe(proc.stdin)
+  outputs.push(proc)
+})
+
+function shutdown () {
+  if (tail) tail.kill()
+  outputs.forEach((o) => { o.kill() })
+}
+
+process.on('SIGINT', shutdown)
+process.on('SIGTERM', shutdown)
